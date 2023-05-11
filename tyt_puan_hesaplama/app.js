4a10705b25db091873d9acae8daac63b50a83d20
yesimdemir <yesimdmr000@gmail.com>
2023-05-11 15:37:50 +0300

    JS ile tyt puan hesaplama
    JS ile dogru yanlis sayilarini alip puan hesaplama uygulamasi

---
+++ tyt_puan_hesaplama/app.js
@@ -0,0 +1,53 @@
+//TYT PUAN HESAPLAMA
+
+
+let turkceDogru , turkceYanlis = 0;
+let matematikDogru , matematikYanlis = 0;
+let sosyalDogru , sosyalYanlis = 0;
+let fenDogru , fenYanlis = 0;
+let puan = 0;
+let okulPuani = 0;
+
+let yeniSatir = "\r\n";
+let mesaj = "TYT Puan Hesaplama Uygulamasına Hoş Geldiniz" + yeniSatir
++ "1-Puan Hesapla" + yeniSatir
++ "2-Çıkış Yap" ;
+
+
+let secim = prompt(mesaj);
+
+switch(secim){
+    case "1":
+
+
+        okulPuani = Number(prompt("Okul puanınızı giriniz: "));
+        turkceDogru = Number(prompt("Türkçe doğru sayısı: "));
+        turkceYanlis = Number(prompt("Türkçe yanlış sayısı: "));
+
+        matematikDogru = Number(prompt("Matematik doğru sayısı: "));
+        matematikYanlis = Number(prompt("Matematik yanlış sayısı: "));
+
+        sosyalDogru = Number(prompt("Sosyal doğru sayısı: "));
+        sosyalYanlis = Number(prompt("Sosyal yanlış sayısı: "));
+
+        fenDogru = Number(prompt("Fen doğru sayısı: "));
+        fenYanlis = Number(prompt("Fen yanlış sayısı: "));
+
+        let dogruSayisi = turkceDogru + matematikDogru + sosyalDogru + fenDogru;
+        let yanlisSayisi = turkceYanlis + matematikYanlis + sosyalYanlis + fenYanlis;
+
+        let kalanDogruSayisi = dogruSayisi - (yanlisSayisi / 4);
+        puan = (kalanDogruSayisi * 4) + 100 + okulPuani;
+        alert("TYT Puanınız: " + puan);
+
+        break;
+
+    case "2":
+        alert("Uygulamadan çıkış yapıldı.");
+        break;
+
+    default:
+        alert("Lütfen geçerli bir işlem seçiniz.");
+        break;
+}
+
